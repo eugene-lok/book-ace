f6ddaa2bb0f7e919ad5bf582ffd5fb8e6756e0f2
Eugene Lok <eugeneywlok@gmail.com>
2021-02-14 20:28:28 -0700

    Configure environment variables

--- application.py
+++ application.py
@@ -4,11 +4,13 @@ from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
+if not app.config.get("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -17,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(app.config.get("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
