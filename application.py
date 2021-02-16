f829651c67be98eb53e7c3440cb89bacd8d5ad89
Eugene Lok <eugeneywlok@gmail.com>
2021-02-15 23:38:48 -0700

    Implement main search form

--- application.py
+++ application.py
@@ -23,10 +23,48 @@ engine = create_engine(app.config.get("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
-def index():
-    return render_template("login.html")
+# Register page 
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
+# Main route
+@app.route("/logged_in", methods = ["GET", "POST"])
+def logged_in():
+    if request.method == "GET":
+        return "Please go back and login."
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Validate login
+        if db.execute("SELECT username FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 1:
+            session["login"] = username
+            return render_template("index.html", username = username, password = password)
+        else:
+            return "Invalid login." 
+        
+
+
+# Search results
+@app.route("/search", methods = ["POST"])
+def search():
+    searchType = request.form.get("searchType")
+    searchQuery = request.form.get("findBook")
+    print(f'searchType{searchType}')
+    print(f'findBook{searchQuery}')
+    if searchType == 'isbn':
+        searchResults = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn", {"isbn": "%" + searchQuery + "%"}).fetchall()
+    elif searchType == 'author':
+        searchResults = db.execute("SELECT * FROM books WHERE author LIKE :author", {"author": "%" + searchQuery + "%"}).fetchall()
+    elif searchType == 'title':
+        searchResults = db.execute("SELECT * FROM books WHERE title LIKE :title", {"title": "%" + searchQuery + "%"}).fetchall()
+    else:
+        return "How did this even happen?"
+    # Validate search result 
+    if searchResults != "[]":
+        return render_template("search.html", searchQuery = searchQuery, searchResults = searchResults)
+    else:
+        return "Search failed."
 
 @app.route("/logged_in", methods = ["POST"])
 def logged_in():
